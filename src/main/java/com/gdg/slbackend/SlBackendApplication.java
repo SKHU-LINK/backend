c773ef94ec2323da2aa835e765a67792d8ad9145
KiwoongKim-1227 <opkiwoong@naver.com>
2025-11-18 15:18:09 +0900

    feat: SKHU-Link 백엔드 기본 패키지 구조 초기 설정

---
+++ src/main/java/com/gdg/slbackend/SlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.slbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdg/slbackend/SlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.slbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
