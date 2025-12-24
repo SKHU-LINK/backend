e30f8df80d39c304f8a99a4012602867d80e9233
nmmn1004 <jjj4120@gmail.com>
2025-12-25 03:03:03 +0900

    Fix: Fix a service of resource return a download url of resource

--- src/main/java/com/gdg/slbackend/api/resource/dto/ResourceDownloadResponse.java
+++ src/main/java/com/gdg/slbackend/api/resource/dto/ResourceDownloadResponse.java
@@ -1,17 +1,11 @@
 package com.gdg.slbackend.api.resource.dto;
 
-import com.gdg.slbackend.domain.resource.Resource;
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@AllArgsConstructor
-@Schema(description = "AWS S3 URL 응답 정보")
-@Setter
 @Builder
 public class ResourceDownloadResponse {
-    private String url;
+    private Long resourceId;
+    private String downloadUrl;
 }
--- src/main/java/com/gdg/slbackend/global/enums/MileageType.java
+++ src/main/java/com/gdg/slbackend/global/enums/MileageType.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum MileageType {
 
     RESOURCE_UPLOAD_REWARD(+10, "자료 업로드 보상"),
-    RESOURCE_DOWNLOAD_UPLOADER_REWARD(+100, "자료 다운로드 보상"),
+    RESOURCE_DOWNLOAD_UPLOADER_REWARD(+20, "자료 다운로드 보상"),
     RESOURCE_DOWNLOAD(-100, "자료 다운로드 차감");
 
     private final int amount;
