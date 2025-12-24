90288d41fdf24609b0e8d41120857e0e97108062
nmmn1004 <jjj4120@gmail.com>
2025-12-24 21:56:12 +0900

    Fix: Fix a comment response

--- src/main/java/com/gdg/slbackend/service/comment/CommentCreator.java
+++ src/main/java/com/gdg/slbackend/service/comment/CommentCreator.java
@@ -3,6 +3,7 @@ package com.gdg.slbackend.service.comment;
 import com.gdg.slbackend.domain.comment.Comment;
 import com.gdg.slbackend.domain.comment.CommentRepository;
 import com.gdg.slbackend.domain.user.User;
+import com.gdg.slbackend.service.user.UserFinder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,19 +15,21 @@ import java.time.LocalDateTime;
 public class CommentCreator {
 
     private final CommentRepository commentRepository;
+    private final UserFinder userFinder;
 
     @Transactional
     public Comment create(Long postId, User author, String content) {
+        User user = userFinder.findByIdOrThrow(author.getId());
+
         Comment comment = Comment.builder()
+                .content(content)
                 .postId(postId)
                 .author(author)
-                .content(content)
                 .likes(0L)
                 .createdAt(LocalDateTime.now())
                 .updatedAt(LocalDateTime.now())
                 .build();
-
+        commentRepository.save(comment);
         return commentRepository.save(comment);
     }
 }
-
